72be02e649413209d33971bb5047f72714ed2e3c
Reimnop <rayngu2005@gmail.com>
2023-11-02 13:13:16 +0000

    Fix authorize bug

--- src/core/command/Authorize.ts
+++ src/core/command/Authorize.ts
@@ -1,18 +1,29 @@
 import { ChatInputCommandInteraction, CacheType } from "discord.js";
 import { Command } from "..";
-import { AuthService } from "../../service";
+import { AuthService, DatabaseService } from "../../service";
 
 export class Authorize implements Command {
     private readonly authService: AuthService;
+    private readonly databaseService: DatabaseService;
     
-    constructor(authService: AuthService) {
+    constructor(authService: AuthService, databaseService: DatabaseService) {
         this.authService = authService;
+        this.databaseService = databaseService;
     }
 
     async execute(interaction: ChatInputCommandInteraction<CacheType>): Promise<void> {
+        await interaction.deferReply({
+            ephemeral: true
+        });
+        const user = await this.databaseService.getUser(interaction.user.id);
+        if (user) {
+            await interaction.editReply({
+                content: "You are already authorized!"
+            });
+            return;
+        }
         const url = this.authService.getAuthUrl(interaction.user.id);
-        await interaction.reply({
-            ephemeral: true,
+        await interaction.editReply({
             content: `Please visit [this link](${url}) to authorize me.`
         });
     }
